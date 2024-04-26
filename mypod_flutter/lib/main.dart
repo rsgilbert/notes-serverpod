46e56271eb03c94d77fb9de640fbd336f98f22fe
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-04-26 07:24:45 +0300

    Implemented dialog for creating new notes

--- mypod_flutter/lib/main.dart
+++ mypod_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:mypod_client/mypod_client.dart';
 import 'package:flutter/material.dart';
+import 'package:mypod_flutter/note_dialog.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
 
 // Sets up a singleton client object that can be used to talk to the server from
@@ -20,11 +21,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Serverpod Demo',
+      title: 'Notes App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Serverpod Example'),
+      home: const MyHomePage(title: 'Notes'),
     );
   }
 }
@@ -39,99 +40,68 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
-  // These fields hold the last result or error message that we've received from
-  // the server or null if no result exists yet.
-  String? _resultMessage;
-  String? _errorMessage;
+  List<Note>? _notes;
+  Exception? _connectionException;
 
-  final _textEditingController = TextEditingController();
+  void _connectionFailed(dynamic exception) {
+    setState(() {
+      _notes = null;
+      _connectionException = exception;
+    });
+  }
 
-  // Calls the `hello` method of the `example` endpoint. Will set either the
-  // `_resultMessage` or `_errorMessage` field, depending on if the call
-  // is successful.
-  void _callHello() async {
+  Future<void> _loadNotes() async {
     try {
-      final result = await client.example.hello(_textEditingController.text);
+      final notes = await client.notes.getAllNotes();
+      print("Notes are $notes with length ${notes.length}");
       setState(() {
-        _errorMessage = null;
-        _resultMessage = result;
+        _notes = notes;
       });
     } catch (e) {
-      setState(() {
-        _errorMessage = '$e';
-      });
+      print("Connection failed $e");
+      _connectionFailed(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print("Initializing state");
+    _loadNotes();
+  }
+
+  Future<void> _createNote(Note note) async {
+    try {
+      await client.notes.createNote(note);
+      await _loadNotes();
+    } catch (e) {
+      _connectionFailed(e);
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: TextField(
-                controller: _textEditingController,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your name',
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: ElevatedButton(
-                onPressed: _callHello,
-                child: const Text('Send to Server'),
-              ),
-            ),
-            _ResultDisplay(
-              resultMessage: _resultMessage,
-              errorMessage: _errorMessage,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// _ResultDisplays shows the result of the call. Either the returned result from
-// the `example.hello` endpoint method or an error message.
-class _ResultDisplay extends StatelessWidget {
-  final String? resultMessage;
-  final String? errorMessage;
-
-  const _ResultDisplay({
-    this.resultMessage,
-    this.errorMessage,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    String text;
-    Color backgroundColor;
-    if (errorMessage != null) {
-      backgroundColor = Colors.red[300]!;
-      text = errorMessage!;
-    } else if (resultMessage != null) {
-      backgroundColor = Colors.green[300]!;
-      text = resultMessage!;
-    } else {
-      backgroundColor = Colors.grey[300]!;
-      text = 'No server response yet.';
-    }
-
-    return Container(
-      height: 50,
-      color: backgroundColor,
-      child: Center(
-        child: Text(text),
-      ),
-    );
+        appBar: AppBar(title: Text(widget.title)),
+        body: _notes == null
+            ? Container()
+            : ListView.builder(
+                itemCount: _notes!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text(_notes![index].text));
+                }),
+        floatingActionButton: _notes == null
+            ? null
+            : FloatingActionButton(
+                onPressed: () {
+                  showNoteDialog(
+                      context: context,
+                      onSaved: (text) {
+                        var note = Note(text: text);
+                        _notes!.add(note);
+                        _createNote(note);
+                      });
+                },
+                child: const Icon(Icons.add)));
   }
 }
